2bdaf2907c38c02a62fcbb4f2134cf25b26dd0b7
Norbert Wojtowicz <norbi2790@gmail.com>
2022-02-16 20:42:50 +0100

    Implement functionality in LoginForm component (users can sign in)

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,47 @@
 import './LoginForm.css';
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import FormError from "../ErrorContainers/FormError/FormError";
+import {useNavigate} from "react-router-dom";
 
 const LoginForm = () => {
+
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    async function login(e) {
+        e.preventDefault();
+        const loginForm = document.querySelector("#login-form");
+        const user = {
+            email: loginForm.email.value,
+            password: loginForm.password.value
+        };
+        if (validateUser(user)) {
+            await axios.post("http://localhost:8080/login", user)
+                .then(res => setTokenAndNavigateToHome(res.headers.authorization))
+                .catch(err => setError(err.response.data.message));
+        }
+    }
+
+    function setTokenAndNavigateToHome(token) {
+        sessionStorage.setItem("token", token);
+        navigate("/");
+    }
+
+    function validateUser(user) {
+        if (user.email.length === 0 || user.password.length === 0) {
+            setError("Żadne z pól nie może być puste"); return false;
+        } else if (user.email.length > 80) {
+            setError("Adres email nie może być dłuższy niż 80 znaków"); return false;
+        } else if (!user.password.match("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")) {
+            setError("Hasło musi zawierac: [Co najmniej 8 znaków, co najmniej jedną małą oraz dużą literę, co najmniej jeden znak specjalny i liczbę]"); return false;
+        } else if (!user.email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)) {
+            setError("Niepoprawny adres email"); return false;
+        }
+        setError("");
+        return true;
+    }
+
     return (
         <div className="wrapper fadeInDown login-form">
             <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
@@ -11,16 +51,18 @@ const LoginForm = () => {
                     <i className="fa fa-user"/> Zaloguj się
                 </div>
 
-                <form>
-                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="Email"/>
-                    <input type="password" id="password" className="fadeIn third" name="login" placeholder="Hasło"/>
-                    <input type="submit" className="fadeIn fourth" value="Zaloguj"/>
+                <form id="login-form">
+                    <input type="text" id="login" className="fadeIn second" name="email" placeholder="Email"/>
+                    <input type="password" id="password" className="fadeIn third" name="password" placeholder="Hasło"/>
+                    <input type="submit" onClick={(e) => login(e)} className="fadeIn fourth" value="Zaloguj"/>
                 </form>
 
                 {/*Forgot password not implemented yet*/}
                 {/*<div id="formFooter">*/}
                 {/*    <a className="underlineHover" href="#">Forgot Password?</a>*/}
                 {/*</div>*/}
+
+                {error.length !== 0 ? <FormError message={error}/> : ""}
 
                 <div id="formFooter">
                     Nie masz konta? <a className="underlineHover" href={"/rejestracja"}>Zarejestruj się</a>
