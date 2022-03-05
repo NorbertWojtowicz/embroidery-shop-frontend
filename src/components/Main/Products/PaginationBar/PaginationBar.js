a9273ecfaa1065d28f28695889214a9ea40e62c1
Norbert Wojtowicz <norbi2790@gmail.com>
2022-03-05 10:41:13 +0100

    Fix PaginationBar bug

--- src/components/Main/Products/PaginationBar/PaginationBar.js
+++ src/components/Main/Products/PaginationBar/PaginationBar.js
@@ -1,5 +1,5 @@
 import './PaginationBar.css';
-import {useEffect, useState} from "react";
+import {useEffect, useState, Fragment} from "react";
 
 const PaginationBar = ({state, setPage}) => {
 
@@ -35,15 +35,45 @@ const PaginationBar = ({state, setPage}) => {
     return (
         <section className="panel blue-wrapper pagination-wrapper top-margin-3">
             <div className="pagination pagination-bar">
-                <li className="page-item previous-btn" onClick={(e) => changePage(e, previousPageReal)}><a className="page-link" href="/">Poprzednia</a></li>
-                <li className="page-item page1" onClick={(e) => changePage(e, 1)}><a className="page-link" href="/">1</a></li>
-                <li className="page-item disabled"><a className="page-link" href="/">...</a></li>
-                <li className={`page-item page${previousPage}`} onClick={(e) => changePage(e, previousPage)}><a className="page-link" href="/">{previousPage}</a></li>
-                <li className={`page-item page${currentPage}`} onClick={(e) => changePage(e, currentPage)}><a className="page-link" href="/">{currentPage}</a></li>
-                <li className={`page-item page${nextPage}`} onClick={(e) => changePage(e, nextPage)}><a className="page-link" href="/">{nextPage}</a></li>
-                <li className="page-item disabled"><a className="page-link" href="/">...</a></li>
-                <li className={`page-item page${totalPages}`} onClick={(e) => changePage(e, totalPages)}><a className="page-link" href="/">{totalPages}</a></li>
-                <li className="page-item next-btn" onClick={(e) => changePage(e, nextPageReal)}><a className="page-link" href="/">Następna</a></li>
+                <li className="page-item previous-btn"
+                    onClick={(e) => changePage(e, previousPageReal)}>
+                    <a className="page-link" href="/">Poprzednia</a></li>
+                <li className="page-item page1"
+                    onClick={(e) => changePage(e, 1)}>
+                    <a className="page-link" href="/">1</a></li>
+                {totalPages > 2 ?
+                    <Fragment>
+                        <li className="page-item disabled"><a className="page-link" href="/">...</a></li>
+                        <li className={`page-item page${previousPage}`}
+                            onClick={(e) => changePage(e, previousPage)}>
+                            <a className="page-link" href="/">{previousPage}</a></li>
+                    </Fragment>
+                        : ""
+                }
+                {totalPages > 3 ?
+                    <li className={`page-item page${currentPage}`}
+                        onClick={(e) => changePage(e, currentPage)}>
+                        <a className="page-link" href="/">{currentPage}</a></li>
+                    : ""
+                }
+                {totalPages > 4 ?
+                    <li className={`page-item page${nextPage}`}
+                        onClick={(e) => changePage(e, nextPage)}>
+                        <a className="page-link" href="/">{nextPage}</a></li>
+                    : ""
+                }
+                {totalPages > 1 ?
+                <Fragment>
+                    <li className="page-item disabled"><a className="page-link" href="/">...</a></li>
+                    <li className={`page-item page${totalPages}`}
+                        onClick={(e) => changePage(e, totalPages)}>
+                        <a className="page-link" href="/">{totalPages}</a></li>
+                </Fragment>
+                    : ""
+                }
+                <li className="page-item next-btn"
+                    onClick={(e) => changePage(e, nextPageReal)}>
+                    <a className="page-link" href="/">Następna</a></li>
             </div>
         </section>
     )
